6b42b11d7ad488215a888b53990fe5a1b7f8f321
Tahsin Ahmed Tushar <tahsintushar.dev@gmail.com>
2024-10-22 21:19:44 +0600

    feat: created utility function to format zod errors

--- src/utilities/response.ts
+++ src/utilities/response.ts
@@ -1,1 +1,10 @@
+import { ZodIssue } from "zod";
 
+export const formatZodErrors = (errors: ZodIssue[]) => {
+  return errors.map((err: ZodIssue) => {
+    return {
+      field: err.path.join("."),
+      message: err.message,
+    };
+  });
+};
